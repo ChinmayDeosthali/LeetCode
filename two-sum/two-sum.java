b41027dee06a883e3fe3322c91b4495a1655f7a6
Chinmay Deosthali <deosthali.chinmay6@gmail.com>
2021-10-14 21:43:53 +0530

    Time: 3 ms (68.08%), Space: 43 MB (6.55%) - LeetHub

--- two-sum/two-sum.java
+++ two-sum/two-sum.java
@@ -1,16 +1,33 @@
 //Two-pass Hash Table solution
+// class Solution {
+// public int[] twoSum(int[] nums, int target) {
+//     Map<Integer, Integer> map = new HashMap<>();
+//     for (int i = 0; i < nums.length; i++) {
+//         map.put(nums[i], i);
+//     }
+//     System.out.println(map);
+//     for (int i = 0; i < nums.length; i++) {
+//         int complement = target - nums[i];
+//         if (map.containsKey(complement) && map.get(complement) != i) {
+//             return new int[] { i, map.get(complement) };
+//         }
+//     }
+//     throw new IllegalArgumentException("No two sum solution");
+// }
+// }
+
+//leetcode one pass hash table
 class Solution {
-public int[] twoSum(int[] nums, int target) {
-    Map<Integer, Integer> map = new HashMap<>();
-    for (int i = 0; i < nums.length; i++) {
-        map.put(nums[i], i);
-    }
-    for (int i = 0; i < nums.length; i++) {
-        int complement = target - nums[i];
-        if (map.containsKey(complement) && map.get(complement) != i) {
-            return new int[] { i, map.get(complement) };
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
         }
+        // In case there is no solution, we'll just return null
+        return null;
     }
-    throw new IllegalArgumentException("No two sum solution");
-}
 }
